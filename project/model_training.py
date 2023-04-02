79495ff72246148eaa18f0c7473f94343184e91a
muradek <muradek@campus.technion.ac.il>
2023-04-02 03:15:07 +0300

    changed dataset and model structure to fit evaluation

--- project/model_training.py
+++ project/model_training.py
@@ -13,19 +13,23 @@ def load_from_roboflow():
     print("finished downloading dataset from roboflow")
     return dataset
 
-# initializing a YOLOv8 model and running it on our dataset
+# initializing a YOLOv8 model and custom train on our dataset
 def set_model(dataset, model_version):
     print("started setting" + model_version)
-    model = YOLO(model_version) # initializing a pre-trained YOLOv8 model # change to yolov8x.pt
+    model = YOLO(model_version) # initializing a pre-trained YOLOv8 model
     data = str(dataset.location) + "/data.yaml"
-    train_results = model.train(data=data, epochs=50, imgsz=640, patience=25, batch=2) # training the model
-    val_results = model.val(data=data) # metrics # evaluate model performance on the validation set
+    train_results = model.train(data=data, epochs=40, imgsz=640, patience=25, batch=2) # training the model
     print("finished YOLO setting")
-    return model, train_results, val_results
+    return model, train_results
+
+def val_model(dataset, model):
+    data = str(dataset.location) + "/data.yaml"
+    val_results = model.val(data=data, save_json=True) # metrics # evaluate model performance on the validation set
+    return val_results
 
 def pred(model, dataset):
     print("started testing")
     source = str(dataset.location) + "/test/images"
-    pred_results = model.predict(source=source, save=True)
+    pred_results = model.predict(source=source, save=True, save_json=True)
     print("finished predicting")
     return pred_results
---
+++ project/preprocess_imgs.py
@@ -0,0 +1,25 @@
+import os
+
+# change directory structure and images names to fit roboflow
+
+# --1-- all images are added to the same directory with unique names
+# annotaion files were adjusted to the new naming
+directory = 'data'
+for batch in os.listdir(directory):
+    for img_name in os.listdir('data/' + batch):
+        old_name = 'data/' + batch + "/" + img_name
+        new_name = "new_imgs/" + batch + "_" + img_name
+        os.rename(old_name, new_name)
+
+
+# --2-- splitting the images to train and test folders
+def in_test_file(img_name): 
+    with open('annotations_test.json') as test_file: 
+         return (img_name in test_file.read())
+
+
+for img_name in os.listdir("new_imgs"):
+    if in_test_file(img_name) :
+        prev_path = "new_imgs/" + img_name
+        new_path = "test_imgs/" + img_name
+        os.rename(prev_path, new_path)
